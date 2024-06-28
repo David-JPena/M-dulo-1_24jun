8ee33f1f5b36b7551eb9eb004380d451afc8abe0
david <jimeenzpenadavid978@gmail.com>
2024-06-28 17:47:33 -0500

    first commit

---
+++ story_content/5bWQ2NOgg95_captions.js
@@ -0,0 +1,4 @@
+window.globalProvideData(
+  "caption",
+  '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.86.32080.0%0D%0A%0D%0A00:00:00.151%20--%3E%2000:00:16.532%0D%0ALlegamos%20al%20punto%20correspondiente%20al%20%20Modelo%20%20de%20integridad,%20que%20es%20el%20conjunto%20de%20orientaciones%20dirigidas%20a%20fortalecer%20la%20capacidad%20preventiva%20y%20defensiva%20de%20las%20entidades%20p%C3%BAblicas%20frente%20a%20la%20corrupci%C3%B3n%20y%20diversas%20pr%C3%A1cticas%20contrarias%20a%20la%20%C3%A9tica.%0D%0A%0D%0A"}'
+);
---
+++ story_content/6aENDKQSjd2_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.86.32080.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.702%0D%0AM%C3%93DULO%201,%20INTEGRIDAD%20P%C3%9ABLICA%0D%0A%0D%0A"}')
